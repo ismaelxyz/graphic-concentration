bf8f747e09394d0ee4c85e40fdb76bc58ecaf2f9
Ismael Belisario <ismaelbeli.com@gmail.com>
2022-06-27 21:24:30 -0400

    update asterlike

--- asterlike/src/main.rs
+++ asterlike/src/main.rs
@@ -1,19 +1,15 @@
-#![feature(crate_visibility_modifier)]
-#![allow(unused, dead_code)]
 mod global;
 mod object;
 mod wrappers;
 
-// text::Text
 use crate::{
-    global::{collision, game_over, hud, init, text_middle, Global, State},
+    global::{game_over, init, text_middle, Global, State},
     object::{Asteroids, Object, Ship, Size, Text},
 };
 use sdl2::{
     event::Event, image::LoadSurface, keyboard::Keycode, rect::Rect, surface::Surface,
     video::FullscreenType,
 };
-//use std::time::Duration;
 
 fn main() {
     // Setup Pre-Game Logic and Constants
@@ -21,27 +17,25 @@ fn main() {
     let (context, mut win) = init();
 
     // Initialize TimerSubsystem
-    let mut time = context.timer().unwrap();
-    let mut global = Global::new();
+    let time = context.timer().unwrap();
 
     win.set_icon(icon);
 
     let mut event_pump = context
         .event_pump()
         .expect("Unable to obtain event pump handle!");
-    win.set_fullscreen(FullscreenType::Desktop);
-    let (w, h) = win.size();
-    global.screen.width = w as i32;
-    global.screen.height = h as i32;
+    win.set_fullscreen(FullscreenType::Desktop).unwrap();
+
+    let mut global = Global::new(win.size());
     let screen = global.screen;
 
-    let mut canvas = win
+    let mut cv = win
         .into_canvas()
         .accelerated()
         .present_vsync()
         .build()
         .expect("Unable to obtain canvas!");
-    let creator = canvas.texture_creator();
+    let creator = cv.texture_creator();
 
     let sprite_sheet =
         wrappers::load_texture("assets/img/sprite-sheet.bmp", &creator, (0x0, 0x0, 0x0));
@@ -59,16 +53,9 @@ fn main() {
         1.0,
     );
 
-    ship.position(
-        (
-            (((screen.width as f32 - screen.right) / 2. + (ship.clip().w / 2) as f32) + screen.left)
-                as i32,
-            (((screen.height as f32 - screen.top) * 0.75 + (ship.clip().h / 2) as f32)
-                + screen.bottom) as i32,
-        ),
-        &mut canvas,
-    );
+    let canvas = &mut cv;
 
+    ship.position((screen.width / 2, screen.height / 2), canvas);
     global.timer.bullet = time.ticks();
 
     while !global.exit {
@@ -102,7 +89,6 @@ fn main() {
         /* Load Event Stack */
         event_pump.pump_events();
 
-        hud(&*ship, &font_large, &global, &mut canvas);
         /* Have a function to update all timers */
         global.timer.game = time.ticks() / 1000; // TODO: El tiempo continua en estando de pausa.
 
@@ -113,30 +99,41 @@ fn main() {
                     continue;
                 }
 
-                ship.update();
-                asteroids.update(screen, &mut global.speed, &mut canvas);
-                collision(&ship, &asteroids);
+                ship.update((
+                    screen,
+                    global.speed,
+                    &mut global.timer,
+                    &event_pump,
+                    canvas,
+                    &time,
+                ));
+                asteroids.update(screen, &mut global.speed, canvas);
+                global.collision(&mut ship, &mut asteroids);
+                global.hud(&*ship, &font_large, canvas);
             }
-            State::Pause => text_middle(
-                Text::new(&font_large, "Pause", Size::Large, 1.0),
-                screen,
-                &mut canvas,
-            ),
+            State::Pause => {
+                text_middle(
+                    Text::new(&font_large, "Pause", Size::Large, 1.0),
+                    screen,
+                    canvas,
+                );
+                global.hud(&*ship, &font_large, canvas);
+            }
             State::Menu => {
-                canvas.clear(); // Remove hud
-                                //
+                //
             }
             _ => unimplemented!(),
         }
 
         canvas.present();
 
-        //std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+        // Frames Per Second Delay
+        global.delay(time.ticks(), &time);
     }
 
     /* Game Over */
     if ship.lives < 1 {
-        game_over(&font_large, screen, &mut canvas);
+        game_over(&font_large, screen, canvas);
     }
 
     unsafe {
