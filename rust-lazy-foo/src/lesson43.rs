49abcf3cbb65409636e581864e2f66bf03ca9dd6
ismaelxyz <ismaelbeli.com@gmail.com>
2022-12-02 02:22:16 -0400

    update lessons 42 and 43

--- rust-lazy-foo/src/lesson43.rs
+++ rust-lazy-foo/src/lesson43.rs
@@ -1,14 +1,10 @@
 use sdl2::{
     event::Event,
-    image::{InitFlag, LoadSurface},
+    image::InitFlag,
     keyboard::Keycode,
     pixels::{Color, PixelFormatEnum},
-    rect::{Rect,Point},
-    render::Texture,
-    surface::Surface
+    rect::{Point, Rect},
 };
-
-
 
 fn main() {
     let sdl_ctx = sdl2::init().expect("Unable to initialize SDL!");
@@ -21,17 +17,16 @@ fn main() {
     sdl2::hint::set("SDL_RENDER_SCALE_QUALITY", "1");
     sdl2::hint::set("SDL_VIDEO_X11_NET_WM_BYPASS_COMPOSITOR", "0");
 
-
     let window = video
         .window("SDL Tutorial 43", 640, 480)
         .position_centered()
         .opengl()
         .build()
         .expect("Could not create SDL window!");
-        //Rotation variables
-let angle = 0f64;
-//SDL_Point screenCenter = { SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 };
-let (screen_width, screen_height) =window.size();
+    //Rotation variables
+    let mut angle = 0f64;
+    let (screen_width, screen_height) = window.size();
+    let screen_center = Point::new(screen_width as i32 / 2, screen_height as i32 / 2);
     let mut canvas = window
         .into_canvas()
         .accelerated()
@@ -40,11 +35,9 @@ let (screen_width, screen_height) =window.size();
         .expect("Unable to obtain canvas!");
 
     let creator = canvas.texture_creator();
-    canvas.set_draw_color(Color::WHITE);
-
-    let (texture_width, texture_height) = (64, 205);
+   
     let mut target_texture = creator
-        .create_texture_target(PixelFormatEnum::RGBA8888, texture_width, texture_height)
+        .create_texture_target(PixelFormatEnum::RGBA8888, screen_width, screen_height)
         .unwrap();
 
     main_loop::setup_mainloop(-1, true, move || {
@@ -59,43 +52,75 @@ let (screen_width, screen_height) =window.size();
             }
         }
 
-    // rotate
-	angle += 2;
-	if( angle > 360 )
-	{
-		angle -= 360;
-	}
-
-         // Set self as render target
-     let result = canvas.with_texture_canvas(&mut target_texture, |texture| {
-         texture_canvas.set_draw_color(Color::RGBA(0xFF, 0xFF, 0xFF, 0xFF));
-         texture_canvas.clear();
-         // texture_canvas.set_draw_color(Color::RGBA(255, 0, 0, 255));
-         // texture_canvas.fill_rect(Rect::new(50, 50, 50, 50)).unwrap();
-     });
-
+        // rotate
+        angle += 2.0;
+        if angle > 360.0 {
+            angle -= 360.0;
+        }
+        
+        canvas.set_draw_color(Color::WHITE);
         canvas.clear();
-        data_stream.paste_pixels(&mut streaming_texture);
 
-        // Render frame
-        canvas.copy_ex(
-            &streaming_texture,
-            None,
-            Some(Rect::new(
-                (screen_width - texture_width) as i32 / 2,
-                (screen_height - texture_height) as i32 / 2,
-                texture_width,
-                texture_height,
-            )),
-            0.0,
-            None,
-            false,
-            false,
-        ).unwrap();
+        // Set self as render target
+        canvas
+            .with_texture_canvas(&mut target_texture, |canvas| {
+                canvas.set_draw_color(Color::RGBA(0xFF, 0xFF, 0xFF, 0xFF));
+                canvas.clear();
+
+                // Render red filled quad
+                let fill_rect = Rect::new(
+                    screen_width as i32 / 4,
+                    screen_height as i32 / 4,
+                    screen_width / 2,
+                    screen_height / 2,
+                );
+                canvas.set_draw_color(Color::RGBA(255, 0, 0, 255));
+                canvas.fill_rect(fill_rect).unwrap();
+
+                // Render green outlined quad
+                let outline_rect = Rect::new(
+                    screen_width as i32 / 6,
+                    screen_height as i32 / 6,
+                    screen_width * 2 / 3,
+                    screen_height * 2 / 3,
+                );
+                canvas.set_draw_color(Color::RGBA(0, 255, 0, 255));
+                canvas.draw_rect(outline_rect).unwrap();
+
+                // Draw blue horizontal line
+                canvas.set_draw_color(Color::RGBA(0, 0, 255, 255));
+                canvas
+                    .draw_line(
+                        (0, screen_height as i32 / 2),
+                        (screen_width as i32, screen_height as i32 / 2),
+                    )
+                    .unwrap();
+
+                // Draw vertical line of yellow dots
+                canvas.set_draw_color(Color::RGBA(255, 255, 0, 255));
+                for i in (0..screen_height as i32).step_by(4) {
+                    canvas.draw_point((screen_width as i32 / 2, i)).unwrap();
+                }
+                
+            })
+            .unwrap();
+
+        // Show rendered to texture
+        let width = target_texture.query().width;
+        let height = target_texture.query().height;
+        canvas
+            .copy_ex(
+                &target_texture,
+                None,
+                Some(Rect::new(0, 0, width, height)),
+                angle,
+                screen_center,
+                false,
+                false,
+            )
+            .unwrap();
 
         canvas.present();
         true
     });
 }
-
-
