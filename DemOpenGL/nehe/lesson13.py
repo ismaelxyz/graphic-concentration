84126c07972951a6aca17cbddea2f30ccf4f6127
ismaelxyz <ismaelbeli.com@gmail.com>
2026-03-08 20:00:12 -0400

    refactor: restructure Lesson 13 to use class-based implementation and improve font handling

--- DemOpenGL/nehe/lesson13.py
+++ DemOpenGL/nehe/lesson13.py
@@ -1,212 +1,155 @@
 #!/usr/bin/env python3
 
+from __future__ import annotations
 
-# This code is not a good example of Python and using OO techniques.  It is a simple and direct
-# exposition of how to use the Open GL API in Python via the PyOpenGL package.  It also uses GLUT,
-# which in my opinion is a high quality library in that it makes my work simpler.  Due to using
-# these APIs, this code is more like a C program using function based programming (which Python
-# is in fact based upon, note the use of closures and lambda) than a "good" OO program.
-#
-# To run this code get and install OpenGL, GLUT, PyOpenGL (see http://www.python.org), and PyNumeric.
-# Installing PyNumeric means having a C compiler that is configured properly, or so I found.  For
-# Win32 this assumes VC++, I poked through the setup.py for Numeric, and chased through disutils code
-# and noticed what seemed to be hard coded preferences for VC++ in the case of a Win32 OS.
-#
-
-from OpenGL.GL import *
-from OpenGL.GLUT import *
-from OpenGL.GLU import *
-from OpenGL.WGL import *    # wglUseFontBitmaps (), wglGetCurrentDC ()
-import win32ui                # CreateFont (), CreateDCFromHandle ()
-from math import cos, sin
 import sys
+import math
+from typing import Any, Optional, Union, cast
+
+import OpenGL.GL as gl
+import OpenGL.GLU as glu
+import OpenGL.GLUT as glut
 
 
-# Some api in the chain is translating the keystrokes to this octal string
-# so instead of saying: ESCAPE = 27, we use the following.
-ESCAPE = b'\033'
+class Lesson13:
+    """NeHe Lesson 13 - Bitmap Fonts (ported from the C/GLUT tutorial code)."""
 
-# Number of the glut window.
-window = 0
-base = None
+    ESCAPE = b"\033"
+
+    def __init__(self) -> None:
+        self.window: int = 0
+
+        self.base_list: Optional[int] = None
+        self.cnt1: float = 0.0
+        self.cnt2: float = 0.0
+
+        # PyOpenGL exposes GLUT bitmap font pointers at runtime, but type stubs often
+        # don't declare these attributes. Using getattr keeps Pylance happy.
+        font = getattr(glut, "GLUT_BITMAP_HELVETICA_18", None)
+        if font is None:
+            font = getattr(glut, "GLUT_BITMAP_8_BY_13", None)
+        if font is None:
+            raise RuntimeError("No GLUT bitmap font available in this environment")
+        self._font: Any = font
+
+    def build_font(self) -> None:
+        base_raw = gl.glGenLists(96)
+        if base_raw is None or int(base_raw) == 0:
+            raise RuntimeError("glGenLists failed while building font")
+        base = int(base_raw)
+        self.base_list = base
+
+        for i in range(96):
+            gl.glNewList(base + i, gl.GL_COMPILE)
+            glut.glutBitmapCharacter(self._font, 32 + i)
+            gl.glEndList()
+
+    def kill_font(self) -> None:
+        if self.base_list is None:
+            return
+
+        gl.glDeleteLists(int(self.base_list), 96)
+        self.base_list = None
+
+    def gl_print(self, text: str) -> None:
+        if not text or self.base_list is None:
+            return
+
+        base = int(self.base_list)
+        for ch in text:
+            code = ord(ch)
+            if 32 <= code < 128:
+                gl.glCallList(base + (code - 32))
+
+    def init_gl(self, width: int, height: int) -> None:
+        gl.glClearColor(0.0, 0.0, 0.0, 0.0)
+        gl.glClearDepth(1.0)
+        gl.glDepthFunc(gl.GL_LESS)
+        gl.glEnable(gl.GL_DEPTH_TEST)
+        gl.glShadeModel(gl.GL_SMOOTH)
+
+        gl.glMatrixMode(gl.GL_PROJECTION)
+        gl.glLoadIdentity()
+        glu.gluPerspective(45.0, float(width) / float(height), 0.1, 100.0)
+
+        gl.glMatrixMode(gl.GL_MODELVIEW)
+
+        self.build_font()
+
+    def resize_scene(self, width: int, height: int) -> None:
+        if height == 0:
+            height = 1
+
+        gl.glViewport(0, 0, width, height)
+        gl.glMatrixMode(gl.GL_PROJECTION)
+        gl.glLoadIdentity()
+        glu.gluPerspective(45.0, float(width) / float(height), 0.1, 100.0)
+        gl.glMatrixMode(gl.GL_MODELVIEW)
+
+    def draw_scene(self) -> None:
+        gl.glClear(int(gl.GL_COLOR_BUFFER_BIT) | int(gl.GL_DEPTH_BUFFER_BIT))
+        gl.glLoadIdentity()
+        gl.glTranslatef(0.0, 0.0, -1.0)
+
+        gl.glColor3f(
+            float(math.cos(self.cnt1)),
+            float(math.sin(self.cnt2)),
+            float(1.0 - 0.5 * math.cos(self.cnt1 + self.cnt2)),
+        )
+
+        gl.glRasterPos2f(
+            float(-0.2 + 0.35 * math.cos(self.cnt1)),
+            float(0.35 * math.sin(self.cnt2)),
+        )
+
+        self.gl_print("OpenGL With NeHe")
+
+        self.cnt1 += 0.01
+        self.cnt2 += 0.0081
+
+        glut.glutSwapBuffers()
+
+    def key_pressed(self, key: Union[bytes, int], *_args) -> None:
+        if key != self.ESCAPE:
+            return
+
+        self.kill_font()
+        if self.window:
+            glut.glutDestroyWindow(int(self.window))
+
+    def _register_callbacks(self) -> None:
+        glut.glutDisplayFunc(self.draw_scene)
+        glut.glutIdleFunc(self.draw_scene)
+        glut.glutReshapeFunc(self.resize_scene)
+        glut.glutKeyboardFunc(self.key_pressed)
+
+    def run(self) -> None:
+        glut.glutInit(sys.argv)
+        glut.glutInitDisplayMode(
+            int(glut.GLUT_RGBA)
+            | int(glut.GLUT_DOUBLE)
+            | int(glut.GLUT_ALPHA)
+            | int(glut.GLUT_DEPTH)
+        )
+        glut.glutInitWindowSize(640, 480)
+        glut.glutInitWindowPosition(0, 0)
+
+        window_raw = glut.glutCreateWindow(
+            "Jeff Molofee's GL Code Tutorial ... NeHe '99"
+        )
+        # Some stubs declare this as Optional[int]; guard before casting.
+        if window_raw is None or int(cast(Any, window_raw)) == 0:
+            raise RuntimeError("glutCreateWindow failed")
+        self.window = int(cast(Any, window_raw))
+        self._register_callbacks()
+
+        glut.glutFullScreen()
+        self.init_gl(640, 480)
+        glut.glutMainLoop()
 
 
-def BuildFont():
-    global base
-
-    wgldc = wglGetCurrentDC()
-    hDC = win32ui.CreateDCFromHandle(wgldc)
-
-    # Storage For 96 Characters, plus 32 at the start...
-    base = glGenLists(32+96)
-
-    # CreateFont () takes a python dictionary to specify the requested font properties.
-    font_properties = {"name": "Courier New",
-                       "width": 0,
-                       "height": -24,
-                       "weight": 800
-                       }
-    font = win32ui.CreateFont(font_properties)
-    # Selects The Font We Want
-    oldfont = hDC.SelectObject(font)
-    # Builds 96 Characters Starting At Character 32
-    wglUseFontBitmaps(wgldc, 32, 96, base+32)
-    # reset the font
-    hDC.SelectObject(oldfont)
-    # Delete The Font (python will cleanup font for us...)
-    return
-
-
-def KillFont():
-    """ Delete The Font List """
-    global base
-    # Delete All 96 Characters
-    glDeleteLists(base, 32+96)
-    return
-
-
-def glPrint(xstr):
-    """ Custom GL "Print" Routine  """
-    global base
-
-    # If THere's No Text Do Nothing
-    if (xstr == None):
-        return
-    if (xstr == ""):
-        return
-
-    glPushAttrib(GL_LIST_BIT)  # Pushes The Display List Bits
-    try:
-        glListBase(base)  # Sets The Base Character to 32
-        glCallLists(xstr)  # Draws The Display List Text
-    finally:
-        glPopAttrib()  # Pops The Display List Bits
-    return
-
-
-# A general OpenGL initialization function.  Sets all of the initial parameters.
-# We call this right after our OpenGL window is created.
-def InitGL(Width, Height):
-    glShadeModel(GL_SMOOTH)                # Enables Smooth Color Shading
-    # This Will Clear The Background Color To Black
-    glClearColor(0.0, 0.0, 0.0, 0.5)
-    glClearDepth(1.0)                    # Enables Clearing Of The Depth Buffer
-    glEnable(GL_DEPTH_TEST)                # Enables Depth Testing
-    glDepthFunc(GL_LEQUAL)                # The Type Of Depth Test To Do
-    # Really Nice Perspective Calculations
-    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST)
-
-    BuildFont()
-    return True
-
-
-# The function called when our window is resized (which shouldn't happen if you enable fullscreen, below)
-def ReSizeGLScene(Width, Height):
-    if Height == 0:                        # Prevent A Divide By Zero If The Window Is Too Small
-        Height = 1
-
-    # Reset The Current Viewport And Perspective Transformation
-    glViewport(0, 0, Width, Height)
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    # field of view, aspect ratio, near and far
-    # This will squash and stretch our objects as the window is resized.
-    gluPerspective(45.0, float(Width)/float(Height), 0.1, 100.0)
-
-    glMatrixMode(GL_MODELVIEW)
-    glLoadIdentity()
-
-
-cnt1 = 0
-cnt2 = 0
-# The main drawing function.
-
-
-def DrawGLScene():
-    global cnt1
-    global cnt2
-
-    # Clear The Screen And The Depth Buffer
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-
-    glLoadIdentity()                    # Reset The Current Modelview Matrix
-    glTranslatef(0.0, 0.0, -1.0)        # Move One Unit Into The Screen
-
-    # Pulsing Colors Based On Text Position
-    glColor3f(1.0 * cos(cnt1), 1.0 * sin(cnt2), 1.0 - 0.5 * cos(cnt1+cnt2))
-    # Position The Text On The Screen
-    glRasterPos2f(-0.45+0.05 * cos(cnt1), 0.32*sin(cnt2))
-    glPrint("Active OpenGL Text With NeHe - %7.2f" %
-            (cnt1))    # Print GL Text To The Screen
-    cnt1 += 0.051  # Increase The First Counter
-    cnt2 += 0.005  # Increase The First Counter
-    glutSwapBuffers()
-    return True
-
-
-# The function called whenever a key is pressed. Note the use of Python tuples to pass in: (key, x, y)
-def keyPressed(*args):
-    global window
-    # If escape is pressed, kill everything.
-    if args[0] == ESCAPE:
-        KillFont()
-        glutDestroyWindow(window)
-
-
-def main():
-    global window
-    # pass arguments to init
-    glutInit(sys.argv)
-
-    # Select type of Display mode:
-    #  Double buffer
-    #  RGBA color
-    # Alpha components supported
-    # Depth buffer
-    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH)
-
-    # get a 640 x 480 window
-    glutInitWindowSize(640, 480)
-
-    # the window starts at the upper left corner of the screen
-    glutInitWindowPosition(0, 0)
-
-    # Okay, like the C version we retain the window id to use when closing, but for those of you new
-    # to Python, remember this assignment would make the variable local and not global
-    # if it weren't for the global declaration at the start of main.
-    window = glutCreateWindow("NeHe's Bitmap Font Tutorial")
-
-    # Register the drawing function with glut, BUT in Python land, at least using PyOpenGL, we need to
-    # set the function pointer and invoke a function to actually register the callback, otherwise it
-    # would be very much like the C version of the code.
-    glutDisplayFunc(DrawGLScene)
-
-    # Uncomment this line to get full screen.
-    # glutFullScreen()
-
-    # When we are doing nothing, redraw the scene.
-    glutIdleFunc(DrawGLScene)
-
-    # Register the function called when our window is resized.
-    glutReshapeFunc(ReSizeGLScene)
-
-    # Register the function called when the keyboard is pressed.
-    glutKeyboardFunc(keyPressed)
-
-    # We've told Glut the type of window we want, and we've told glut about
-    # various functions that we want invoked (idle, resizing, keyboard events).
-    # Glut has done the hard work of building up thw windows DC context and
-    # tying in a rendering context, so we are ready to start making immediate mode
-    # GL calls.
-    # Call to perform inital GL setup (the clear colors, enabling modes, and most releveant -
-    # consturct the displays lists for the bitmap font.
-    InitGL(640, 480)
-
-    # Print message to console, and kick off the main to get it rolling.
-    print('Hit ESC key to quit.')
-
-    # Start Event Processing Engine
-    glutMainLoop()
+def main() -> None:
+    Lesson13().run()
 
 
 if __name__ == "__main__":
