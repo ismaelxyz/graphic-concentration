fc6af6ae6dcf0792ecfbdbcd018a587ccb8b45b6
ismaelxyz <ismaelbeli.com@gmail.com>
2026-03-14 17:07:12 -0400

    refactor: update checker.py to use consistent OpenGL and Tk imports

--- DemOpenGL/tom/checker.py
+++ DemOpenGL/tom/checker.py
@@ -1,49 +1,50 @@
 #!/usr/bin/python
 
-from OpenGL.GL import *
-from OpenGL.GLUT import *
-from OpenGL.Tk import *
+import OpenGL.GL as gl
+import OpenGL.GLUT as glut
+import OpenGL.Tk as tk
 import sys
 
 
 def redraw(_):
-    glClearColor(1, 0, 1, 0)
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    glColor3f(0, 1, 0)
+    gl.glClearColor(1, 0, 1, 0)
+    gl.glClear(int(gl.GL_COLOR_BUFFER_BIT) | int(gl.GL_DEPTH_BUFFER_BIT))
+    gl.glColor3f(0, 1, 0)
     # draw checkerboard
     N = 4
-    glDisable(GL_LIGHTING)
+    gl.glDisable(gl.GL_LIGHTING)
     for x in range(-N, N):
         for y in range(-N, N):
             if (x + y) % 2 == 0:
-                glColor3f(1, 1, 1)
+                gl.glColor3f(1, 1, 1)
             else:
-                glColor3f(0, 0, 0)
-            glRectf(x, y, x + 1, y + 1)
-    glEnable(GL_LIGHTING)
+                gl.glColor3f(0, 0, 0)
+            gl.glRectf(x, y, x + 1, y + 1)
+    gl.glEnable(gl.GL_LIGHTING)
 
-    glPushMatrix()
-    glTranslatef(0., 0., 1.)
-    glutSolidSphere(1.0, 20, 20)
-    glPopMatrix()
+    gl.glPushMatrix()
+    gl.glTranslatef(0.0, 0.0, 1.0)
+    glut.glutSolidSphere(1.0, 20, 20)
+    gl.glPopMatrix()
 
 
 def main():
-    frame = Frame()
-    frame.pack(side='top')
-    gl = Opengl(width=200, height=200, double=1, depth=1)
-    glutInit([])
-    gl.redraw = redraw
-    quit = Button(frame, text='Quit', command=sys.exit)
-    quit.pack({'side': 'top', 'side': 'left'})
-    help = Button(frame, text='Help', command=gl.help)
-    help.pack({'side': 'top', 'side': 'left'})
-    reset = Button(frame, text='Reset', command=gl.reset)
-    reset.pack({'side': 'top', 'side': 'left'})
-    gl.pack(side='top', expand=1, fill='both')
-    gl.set_eyepoint(20.)
-    gl.mainloop()
+    frame = tk.Frame()
+    frame.pack(side="top")
+    gltk = tk.Opengl(width=200, height=200, double=1, depth=1)
+    glut.glutInit([])
+    gltk.redraw = redraw
+    quit = tk.Button(frame, text="Quit", command=sys.exit)
+    quit.pack({"side": "top", "side": "left"})
+    help = tk.Button(frame, text="Help", command=gltk.help)
+    help.pack({"side": "top", "side": "left"})
+    reset = tk.Button(frame, text="Reset", command=gltk.reset)
+    reset.pack({"side": "top", "side": "left"})
+
+    gltk.pack(side="top", expand=1, fill="both")
+    gltk.set_eyepoint(20.0)
+    gltk.mainloop()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
