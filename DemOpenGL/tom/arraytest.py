e80604cb88c75341bd656a0b0f85ba340be966b2
ismaelxyz <ismaelbeli.com@gmail.com>
2026-03-14 17:00:03 -0400

    refactor: update arraytest.py to use numpy and OpenGL imports consistently

--- DemOpenGL/tom/arraytest.py
+++ DemOpenGL/tom/arraytest.py
@@ -1,48 +1,48 @@
 #!/usr/bin/python
 
-from numpy import *
-from numpy.random import *
-from OpenGL.Tk import *
-from OpenGL.GL import *
+import numpy as np
+import numpy.random as random
+import OpenGL.Tk as tk
+import OpenGL.GL as gl
 import sys
 
-N = 50
+N_POINTS = 50
 
 
 def shuffle(a, b):
-    return ravel(transpose(reshape(concatenate([a, b]), (2, len(a)))))
+    return np.ravel(np.transpose(np.reshape(np.concatenate([a, b]), (2, len(a)))))
 
 
 def redraw(_):
-    glClearColor(1, 1, 1, 0)
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    glOrtho(-1, 1, -1, 1, -1, 1)
-    glDisable(GL_LIGHTING)
-    glDrawArrays(GL_LINE_LOOP, 0, N)
-    glEnable(GL_LIGHTING)
+    gl.glClearColor(1, 1, 1, 0)
+    gl.glClear(int(gl.GL_COLOR_BUFFER_BIT) | int(gl.GL_DEPTH_BUFFER_BIT))
+    gl.glOrtho(-1, 1, -1, 1, -1, 1)
+    gl.glDisable(gl.GL_LIGHTING)
+    gl.glDrawArrays(gl.GL_LINE_LOOP, 0, N_POINTS)
+    gl.glEnable(gl.GL_LIGHTING)
 
 
 def main():
-    frame = Frame()
-    frame.pack(side='top', expand=1)
-    quit = Button(frame, text='Quit', command=sys.exit)
-    quit.pack(side='top')
-    gl = Opengl(width=400, height=400, double=1)
-    a = arange(0, N)
-    vertex = shuffle(cos(2 * pi * a / N), sin(2 * pi * a / N))
-    vertex.shape = (N, 2)
-    color = random(N * 3)
-    color.shape = (N, 3)
+    frame = tk.Frame()
+    frame.pack(side="top", expand=1)
+    quit = tk.Button(frame, text="Quit", command=sys.exit)
+    quit.pack(side="top")
+    gltk = tk.Opengl(width=400, height=400, double=1)
+    a = np.arange(0, N_POINTS)
+    vertex = shuffle(np.cos(2 * np.pi * a / N_POINTS), np.sin(2 * np.pi * a / N_POINTS))
+    vertex.shape = (N_POINTS, 2)
+    color = random.random(N_POINTS * 3)
+    color.shape = (N_POINTS, 3)
 
-    glVertexPointerd(vertex)
-    glColorPointerd(color)
-    glEnableClientState(GL_VERTEX_ARRAY)
-    glEnableClientState(GL_COLOR_ARRAY)
+    gl.glVertexPointerd(vertex)
+    gl.glColorPointerd(color)
+    gl.glEnableClientState(gl.GL_VERTEX_ARRAY)
+    gl.glEnableClientState(gl.GL_COLOR_ARRAY)
 
-    gl.redraw = redraw
-    gl.pack(side='top', expand=1, fill='both')
-    gl.mainloop()
+    gltk.redraw = redraw
+    gltk.pack(side="top", expand=1, fill="both")
+    gltk.mainloop()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
